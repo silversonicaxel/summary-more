1fd83b913c6145ea74f303bf8df93743f598f8ec
Alessandro Rabitti <axel83.beta@gmail.com>
2019-03-22 23:48:24 +0100

    Write all test for applySummaryMore function

--- src/class/maker.spec.ts
+++ src/class/maker.spec.ts
@@ -2,7 +2,7 @@ import { Maker } from './maker'
 import { assert, createSandbox, spy } from 'sinon'
 import * as util from 'util'
 import * as fs from 'fs'
-import { expect } from 'chai';
+import { expect } from 'chai'
 import * as path from 'path'
 
 describe('#Maker', () => {
@@ -42,12 +42,51 @@ describe('#Maker', () => {
       expect(maker['summaryFileFolder']).to.equal(path.resolve(baseFolder))
     })
 
+    it('should read summary file with no error', async () => {
+      const readSummaryFileStub = sandboxSet.stub(maker, 'readSummaryFile').returns('Some content')
+      sandboxSet.stub(maker, 'readFilesFromFolder')
+      const consoleStub = sandboxSet.stub(console, 'error');
+      const processStub = sandboxSet.stub(process, 'exit');
+
+      await maker.applySummaryMore(baseFolder, docsFolder, docsSection)
+
+      assert.calledOnce(readSummaryFileStub)
+      assert.calledWith(readSummaryFileStub, `${baseFolder}/${maker['summaryFileName']}`)
+      assert.notCalled(consoleStub)
+      assert.notCalled(processStub)
+    })
+
+    it('should read summary file with an error', async () => {
+      const readSummaryFileStub = sandboxSet.stub(maker, 'readSummaryFile').returns('')
+      sandboxSet.stub(maker, 'readFilesFromFolder')
+      const consoleStub = sandboxSet.stub(console, 'error');
+      const processStub = sandboxSet.stub(process, 'exit');
+
+      await maker.applySummaryMore(baseFolder, docsFolder, docsSection)
+
+      assert.calledOnce(readSummaryFileStub)
+      assert.calledWith(readSummaryFileStub, `${baseFolder}/${maker['summaryFileName']}`)
+      assert.calledOnce(consoleStub)
+      assert.calledWith(consoleStub, maker['errorNoSummaryFile'])
+      assert.calledOnce(processStub)
+      assert.calledWith(processStub, 1)
+    })
+
     it('should initialize section', async () => {
       sandboxSet.stub(maker, 'readSummaryFile').returns('Some content')
       sandboxSet.stub(maker, 'readFilesFromFolder')
       await maker.applySummaryMore(baseFolder, docsFolder, docsSection)
 
       expect(maker['summaryFileSection']).to.equal(docsSection)
+    })
+
+    it('should read documents from folder', async () => {
+      sandboxSet.stub(maker, 'readSummaryFile').returns('Some content')
+      const readFilesFromFolderStub = sandboxSet.stub(maker, 'readFilesFromFolder')
+      await maker.applySummaryMore(baseFolder, docsFolder, docsSection)
+
+      assert.calledOnce(readFilesFromFolderStub)
+      assert.calledWith(readFilesFromFolderStub, maker['summaryDocumentationFolder'], maker['updateSummaryFile'])
     })
   })
 })

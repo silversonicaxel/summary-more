4d8c7c4440475a41632368273cd9f5ced5ddb697
Alessandro Rabitti <axel83.beta@gmail.com>
2019-03-27 22:16:52 +0100

    Bump to version 2.0.0

--- src/class/configurer.ts
+++ src/class/configurer.ts
@@ -13,7 +13,7 @@ export class Configurer {
   }
 
   private setupOptions(): void {
-    const version = '1.0.0'
+    const version = '2.0.0'
 
     program
       .version(version, '-v, --version')

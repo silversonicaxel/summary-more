007c56a3dee129f91b586ca950423b9e12d54de7
Alessandro Rabitti <axel83.beta@gmail.com>
2019-04-04 21:08:44 +0200

    Fix bug on second spaces in folders or names of documents

--- src/class/maker.ts
+++ src/class/maker.ts
@@ -129,7 +129,7 @@ export class Maker {
 
     readFiles = readFiles.map((readFile: string) => {
       const filePath = readFile.replace(`${this.summaryFileFolder}/`, '')
-      return `* [${path.parse(filePath).name}](${filePath.replace(' ', '&#32;')})`
+      return `* [${path.parse(filePath).name}](${filePath.replace(/[ ]/g, '&#32;')})`
     })
 
     this.handleSummaryContent(this.getRowsFromFileContent(this.summaryFileContent), readFiles)

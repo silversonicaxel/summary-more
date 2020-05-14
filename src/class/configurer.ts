0bf2367a7b8457ee0fbfb137169d2b55b73e320f
Alessandro Rabitti <axel83.beta@gmail.com>
2020-05-14 22:56:08 +0200

    Bump to version 3.1.3

--- src/class/configurer.ts
+++ src/class/configurer.ts
@@ -13,7 +13,7 @@ export class Configurer {
   }
 
   private setupOptions(): void {
-    const version = '3.1.2'
+    const version = '3.1.3'
 
     program
       .version(version, '-v, --version')
